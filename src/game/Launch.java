21b2329929b07a55e9f8a391960cea049698fb44
andymuzo <rogersa@sandymoorschool.org.uk>
2016-10-01 12:17:20 +0100

    Added the initial folder structure
    Divided the code up into seperate elements for the game engine, the
    rendering, agents (i.e. game objects) and factories.

---
+++ src/game/Launch.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// This is where the program first launches to,
+		// we only use this as a way of starting up the program.
+		
+		// At first this will launch the first level directly but
+		// as we progress it will launch options and menus instead
+		
+		// ***Launch Program Here***
+	}
+
+}
---
+++ src/game/agents/enemies/Enemy.java
@@ -0,0 +1,10 @@
+package game.agents.enemies;
+
+/**
+ * This is the interface that all enemy units will implement
+ * @author rogersa
+ *
+ */
+public interface Enemy {
+
+}
